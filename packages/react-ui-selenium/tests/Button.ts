ecc75b22e06bb63c479beb99658c2298e783897c
wKich <nflazzer@gmail.com>
2019-05-31 18:21:35 +0500

    approve Button images

--- packages/react-ui-selenium/tests/Button.ts
+++ packages/react-ui-selenium/tests/Button.ts
@@ -68,11 +68,7 @@ describe('Button', function() {
         .move({
           origin: this.browser.findElement(By.css('button')),
         })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
+        .click(this.browser.findElement(By.css('button')))
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('clicked');
     });
@@ -91,11 +87,7 @@ describe('Button', function() {
         .move({
           origin: this.browser.findElement(By.css('button')),
         })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
+        .click(this.browser.findElement(By.css('button')))
         .click(this.browser.findElement(By.css('body')))
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('clickedOutside');
@@ -106,21 +98,6 @@ describe('Button', function() {
         .actions({
           bridge: true,
         })
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('body')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
-        .click(this.browser.findElement(By.css('body')))
         .sendKeys(Key.TAB)
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('tabPress');
@@ -192,11 +169,7 @@ describe('Button', function() {
         .move({
           origin: this.browser.findElement(By.css('button')),
         })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
+        .click(this.browser.findElement(By.css('button')))
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('clicked');
     });
@@ -215,11 +188,7 @@ describe('Button', function() {
         .move({
           origin: this.browser.findElement(By.css('button')),
         })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
+        .click(this.browser.findElement(By.css('button')))
         .click(this.browser.findElement(By.css('body')))
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('clickedOutside');
@@ -230,21 +199,6 @@ describe('Button', function() {
         .actions({
           bridge: true,
         })
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('body')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
-        .click(this.browser.findElement(By.css('body')))
         .sendKeys(Key.TAB)
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('tabPress');
@@ -316,11 +270,7 @@ describe('Button', function() {
         .move({
           origin: this.browser.findElement(By.css('button')),
         })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
+        .click(this.browser.findElement(By.css('button')))
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('clicked');
     });
@@ -339,11 +289,7 @@ describe('Button', function() {
         .move({
           origin: this.browser.findElement(By.css('button')),
         })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
+        .click(this.browser.findElement(By.css('button')))
         .click(this.browser.findElement(By.css('body')))
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('clickedOutside');
@@ -354,21 +300,6 @@ describe('Button', function() {
         .actions({
           bridge: true,
         })
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('body')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
-        .click(this.browser.findElement(By.css('body')))
         .sendKeys(Key.TAB)
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('tabPress');
@@ -440,15 +371,12 @@ describe('Button', function() {
         .move({
           origin: this.browser.findElement(By.css('button')),
         })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
+        .click(this.browser.findElement(By.css('button')))
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('clicked');
     });
     it('clickedOutside', async function() {
+      // FIXME: Don't pass in FF
       const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
@@ -463,11 +391,7 @@ describe('Button', function() {
         .move({
           origin: this.browser.findElement(By.css('button')),
         })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
+        .click(this.browser.findElement(By.css('button')))
         .click(this.browser.findElement(By.css('body')))
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('clickedOutside');
@@ -478,21 +402,6 @@ describe('Button', function() {
         .actions({
           bridge: true,
         })
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('body')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
-        .click(this.browser.findElement(By.css('body')))
         .sendKeys(Key.TAB)
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('tabPress');
@@ -564,11 +473,7 @@ describe('Button', function() {
         .move({
           origin: this.browser.findElement(By.css('button')),
         })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
+        .click(this.browser.findElement(By.css('button')))
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('clicked');
     });
@@ -587,11 +492,7 @@ describe('Button', function() {
         .move({
           origin: this.browser.findElement(By.css('button')),
         })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
+        .click(this.browser.findElement(By.css('button')))
         .click(this.browser.findElement(By.css('body')))
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('clickedOutside');
@@ -602,21 +503,6 @@ describe('Button', function() {
         .actions({
           bridge: true,
         })
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('body')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .press()
-        .move({
-          origin: this.browser.findElement(By.css('button')),
-        })
-        .release()
-        .click(this.browser.findElement(By.css('body')))
         .sendKeys(Key.TAB)
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('tabPress');
@@ -648,39 +534,20 @@ describe('Button', function() {
   });
   describe('different visual states', function() {
     it('page - 1', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 1');
     });
     it('page - 2', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
         })
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 2');
     });
     it('page - 3', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -688,16 +555,10 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 3');
     });
     it('page - 4', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -706,16 +567,10 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 4');
     });
     it('page - 5', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -725,16 +580,10 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 5');
     });
     it('page - 6', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -745,16 +594,10 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 6');
     });
     it('page - 7', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -766,16 +609,10 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 7');
     });
     it('page - 8', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -788,16 +625,10 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 8');
     });
     it('page - 9', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -811,44 +642,26 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 9');
     });
   });
   describe('different content', function() {
     it('page - 1', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 1');
     });
     it('page - 2', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
         })
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 2');
     });
     it('page - 3', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -856,16 +669,10 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 3');
     });
     it('page - 4', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -874,16 +681,10 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 4');
     });
     it('page - 5', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -893,16 +694,10 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 5');
     });
     it('page - 6', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -913,16 +708,10 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 6');
     });
     it('page - 7', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -934,16 +723,10 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 7');
     });
     it('page - 8', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#paginator'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
       await this.browser
         .actions({
           bridge: true,
@@ -956,6 +739,7 @@ describe('Button', function() {
         .click(this.browser.findElement(By.css('#next-page')))
         .click(this.browser.findElement(By.css('#next-page')))
         .perform();
+      const element = await this.browser.findElement(By.css('[data-comp-name~="ComponentTable"]'));
       await expect(await element.takeScreenshot()).to.matchImage('page - 8');
     });
   });
