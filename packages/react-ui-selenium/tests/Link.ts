16a1d782b23539cf60c48afcc70bee12086cfca0
wKich <nflazzer@gmail.com>
2019-06-18 10:48:47 +0500

    approve Link images

--- packages/react-ui-selenium/tests/Link.ts
+++ packages/react-ui-selenium/tests/Link.ts
@@ -19,21 +19,6 @@ describe('Link', function() {
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('hover');
     });
-    it('mouseLeave', async function() {
-      const element = await this.browser.findElement(By.css('#test-element'));
-      await this.browser
-        .actions({
-          bridge: true,
-        })
-        .move({
-          origin: this.browser.findElement(By.css('a')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('body')),
-        })
-        .perform();
-      await expect(await element.takeScreenshot()).to.matchImage('mouseLeave');
-    });
   });
   describe('With Icon', function() {
     it('idle', async function() {
@@ -51,21 +36,6 @@ describe('Link', function() {
         })
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('hover');
-    });
-    it('mouseLeave', async function() {
-      const element = await this.browser.findElement(By.css('#test-element'));
-      await this.browser
-        .actions({
-          bridge: true,
-        })
-        .move({
-          origin: this.browser.findElement(By.css('a')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('body')),
-        })
-        .perform();
-      await expect(await element.takeScreenshot()).to.matchImage('mouseLeave');
     });
   });
   describe('Danger', function() {
@@ -85,21 +55,6 @@ describe('Link', function() {
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('hover');
     });
-    it('mouseLeave', async function() {
-      const element = await this.browser.findElement(By.css('#test-element'));
-      await this.browser
-        .actions({
-          bridge: true,
-        })
-        .move({
-          origin: this.browser.findElement(By.css('a')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('body')),
-        })
-        .perform();
-      await expect(await element.takeScreenshot()).to.matchImage('mouseLeave');
-    });
   });
   describe('Grayed', function() {
     it('idle', async function() {
@@ -118,21 +73,6 @@ describe('Link', function() {
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('hover');
     });
-    it('mouseLeave', async function() {
-      const element = await this.browser.findElement(By.css('#test-element'));
-      await this.browser
-        .actions({
-          bridge: true,
-        })
-        .move({
-          origin: this.browser.findElement(By.css('a')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('body')),
-        })
-        .perform();
-      await expect(await element.takeScreenshot()).to.matchImage('mouseLeave');
-    });
   });
   describe('Disabled', function() {
     it('idle', async function() {
@@ -150,21 +90,6 @@ describe('Link', function() {
         })
         .perform();
       await expect(await element.takeScreenshot()).to.matchImage('hover');
-    });
-    it('mouseLeave', async function() {
-      const element = await this.browser.findElement(By.css('#test-element'));
-      await this.browser
-        .actions({
-          bridge: true,
-        })
-        .move({
-          origin: this.browser.findElement(By.css('a')),
-        })
-        .move({
-          origin: this.browser.findElement(By.css('body')),
-        })
-        .perform();
-      await expect(await element.takeScreenshot()).to.matchImage('mouseLeave');
     });
   });
 });
