7521113cb7d4f9814af4a5f2c17f580f3083678a
wKich <nflazzer@gmail.com>
2019-06-18 10:52:19 +0500

    approve Logotype images

--- packages/react-ui-selenium/tests/Logotype.ts
+++ packages/react-ui-selenium/tests/Logotype.ts
@@ -4,23 +4,11 @@ import { By } from 'selenium-webdriver';
 describe('Logotype', function() {
   describe('with widget', function() {
     it('without widget', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#toggle-widget'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
+      const element = await this.browser.findElement(By.css('[data-comp-name~="Logotype"'));
       await expect(await element.takeScreenshot()).to.matchImage('without widget');
     });
     it('with widget', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: ['#toggle-widget'],
-        },
-      ];
-      const element = await this.browser.findElement(By.css('#test-element'));
+      const element = await this.browser.findElement(By.css('[data-comp-name~="Logotype"'));
       await this.browser
         .actions({
           bridge: true,
