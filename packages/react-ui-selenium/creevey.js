dc3fe86c6c3f25c1ddfe336fa6ef541030acbc73
wKich <nflazzer@gmail.com>
2019-06-20 18:18:14 +0500

    update creevey, add flat browsers

--- packages/react-ui-selenium/creevey.js
+++ packages/react-ui-selenium/creevey.js
@@ -1,17 +1,57 @@
 require('ts-node').register({ files: true, transpileOnly: true });
 
+const defaultAddress = {
+  host: 'localhost',
+  port: 6060,
+  path: '/iframe.html',
+};
+
+const flatComponents = ['Button', 'Checkbox', 'Input', 'Radio', 'Textarea', 'Toggle', 'InputLikeText', 'Switcher'];
+const flatRegex = new RegExp(`(${flatComponents.join('|')})\\.ts$`);
+
 const config = {
   gridUrl: 'http://screen:shot@grid.testkontur.ru/wd/hub',
-  address: {
-    host: 'localhost',
-    port: 6060,
-    path: '/iframe.html',
-  },
+  address: defaultAddress,
   testRegex: /\.ts$/,
   browsers: {
-    chrome: { browserName: 'chrome', limit: 2 },
+    chrome: { browserName: 'chrome' },
+    chromeFlat: {
+      browserName: 'chrome',
+      testRegex: flatRegex,
+      address: {
+        ...defaultAddress,
+        port: 6061,
+      },
+    },
     firefox: { browserName: 'firefox' },
-    ie11: { browserName: 'internet explorer', limit: 5 },
+    firefoxFlat: {
+      browserName: 'firefox',
+      testRegex: flatRegex,
+      address: {
+        ...defaultAddress,
+        port: 6061,
+      },
+    },
+    ie11: {
+      browserName: 'internet explorer',
+      // NOTE Enable after switch new separate pool for IE
+      // 'se:ieOptions': {
+      //   enablePersistentHover: true,
+      //   nativeEvents: true,
+      //   requireWindowFocus: true,
+      //   'ie.usePerProcessProxy': true,
+      //   'ie.browserCommandLineSwitches': '-private',
+      //   'ie.ensureCleanSession': true,
+      // },
+    },
+    ie11Flat: {
+      browserName: 'internet explorer',
+      testRegex: flatRegex,
+      address: {
+        ...defaultAddress,
+        port: 6061,
+      },
+    },
   },
 };
 
