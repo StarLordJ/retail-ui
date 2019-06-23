38310b13085cddbf171cfa8cc81f351a1a562e3c
Зырянова Елена Викторовна <zyryanova@kontur>
2019-06-23 13:56:57 +0500

    test(Spinner): approve images

--- packages/react-ui-selenium/tests/Spinner.ts
+++ packages/react-ui-selenium/tests/Spinner.ts
@@ -4,32 +4,12 @@ import { By } from 'selenium-webdriver';
 describe('Spinner', function() {
   describe('Normal', function() {
     it('plain', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: [
-            {
-              every: "[class^='Spinner-inner']",
-            },
-          ],
-        },
-      ];
       const element = await this.browser.findElement(By.css('#test-element'));
       await expect(await element.takeScreenshot()).to.matchImage('plain');
     });
   });
   describe('With ReactNode in caption', function() {
     it('plain', async function() {
-      [
-        {
-          type: 'ignoreElements',
-          args: [
-            {
-              every: "[class^='Spinner-inner']",
-            },
-          ],
-        },
-      ];
       const element = await this.browser.findElement(By.css('#test-element'));
       await expect(await element.takeScreenshot()).to.matchImage('plain');
     });
